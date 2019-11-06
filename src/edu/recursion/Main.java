b6acb592557267d1bd9e1e32dd504eff59b9cb06
Josiah S Greenwell <jsgreenwell83@gmail.com>
2019-11-06 14:19:01 -0500

    Added few other math functions

--- src/edu/recursion/Main.java
+++ src/edu/recursion/Main.java
@@ -5,6 +5,8 @@ public class Main {
     public static void main(String[] args) {
       MathRecursion mr = new MathRecursion();
 
+      System.out.println(mr.sumSeries(8));
+      System.out.println(mr.computePowers(4, 3));
       System.out.println(mr.factorial(10));
 
       System.out.println(ReverseString.reverse());
--- src/edu/recursion/MathRecursion.java
+++ src/edu/recursion/MathRecursion.java
@@ -2,7 +2,35 @@ package edu.recursion;
 
 public class MathRecursion {
 
+  /**
+   * Calculate the sum of a series of numbers starting at 1
+   * @param number The final number to add to
+   * @return The number or the number plus the recursive function
+   */
+  protected int sumSeries(final int number) {
+    if (number > 0) {
+      return number + sumSeries(number - 1);
+    } else {
+      return number;
+    }
+  }
 
+  /**
+   * Calculate the answer to a number raised to a provided power
+   * @param number The number to multiple (by itself to exp)
+   * @param exp The exponent to raise the number to
+   * @return On 0: 1 (n^0 = 1), 1: return number, other: return function
+   */
+  protected long computePowers(final int number, final int exp) {
+    switch (exp) {
+      case 0:
+        return 1;
+      case 1:
+        return number;
+      default:
+        return number * computePowers(number, exp - 1);
+    }
+  }
   /**
    * This is probably the most frequently used example
    * Function, through its variable assignment, is passed an integer (4)
@@ -14,7 +42,7 @@ public class MathRecursion {
    * @param number A number (note too high will cause a stack overflow)
    * @return number * number - 1 or 1 (if number is 0)
    */
-  protected long factorial(int number)
+  protected long factorial(final int number)
   {
     if (number == 1)
         return 1;
