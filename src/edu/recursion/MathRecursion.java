003dc1fb92d3e383b04d37565e70c3062dec99a6
Josiah Greenwell <jsgreenwell@users.noreply.github.com>
2020-11-25 11:08:19 -0500

    Update MathRecursion.java
    Remove to re-add with pull request to show how.

--- src/edu/recursion/MathRecursion.java
+++ src/edu/recursion/MathRecursion.java
@@ -35,25 +35,6 @@ public class MathRecursion {
   }
 
   /**
-   * This is probably the most frequently used example
-   * Function, through its variable assignment, is passed an integer (4)
-   *   which it evaluates and returns 1 if it is 0
-   *   or else it returns the current value
-   *     (i) * another calculation made by recalling the function with the number - 1
-   * This continues until the passed value is decremented to 0 (where it returns 1).
-   *
-   * @param number A number (note too high will cause a stack overflow)
-   * @return number * number - 1 or 1 (if number is 0)
-   */
-  protected long factorial(final int number)
-  {
-    if (number == 1)
-        return 1;
-    else
-        return number * factorial(number-1);
-  }
-
-  /**
    * So you can time & see the difference yourselves
    * Function is passed to stopping number for the factorial
    *   which it uses as the stop point of a for loop which
